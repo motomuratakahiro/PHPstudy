2934e46c995109a082e7af341d98e054eb152784
motomura <43534770+motomuratakahiro@users.noreply.github.com>
2019-05-23 01:56:05 +0900

    書籍リスト

---
+++ bookapp/routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Book;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('books');
+});
+*/
+Route::group(['middleware' => ['web']], function () {
+	Route::get('/', ['middleware' =>  'auth',function(){
+		$books = Book::all();
+		return view('books',[
+			'books' => $books
+		]);
+	}]);
+
+	Route::post('/book',['middleware' =>  'auth',function(Request $request) {
+		$validator = Validator::make($request->all(), [
+			'name' => 'required|max:255',
+	    ]);
+		
+
+		if($validator->fails()) {
+			return redirect('/')
+			->withInput()
+			->withErrors($validator);
+		}
+
+		$book = new Book; // ORM
+		$book->title = $request->name;
+		$book->save();
+
+		return redirect('/');
+	}]);
+
+	Route::delete('/book/{book}',['middleware' =>  'auth', function(Book $book) {
+		$book->delete();
+
+		return redirect('/');
+	}]);
+	
+	Route::auth();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
